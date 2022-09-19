9e92a19d6592a726c280925de98ce32cf814d218
al3xvndra <taal21rq@student.ju.se>
2022-09-19 13:47:03 +0200

    Adding posts works

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const data = require("./data");
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(express.static("public"));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 app.engine(
   "hbs",
@@ -32,6 +41,20 @@ app.get("/create", function (request, response) {
   response.render("create.hbs", model);
 });
 
+app.post("/create", function (request, response) {
+  const contentDate = request.body.contentDate;
+  const contentTitle = request.body.contentTitle;
+  const content = request.body.content;
+
+  data.posts.unshift({
+    postId: data.posts.at(-1).id + 1,
+    date: contentDate,
+    title: contentTitle,
+    content: content,
+  });
+  response.redirect("/posts");
+});
+
 app.get("/contact", function (request, response) {
   const model = {
     contact: data.contact,
@@ -45,7 +68,5 @@ app.get("/logIn", function (request, response) {
   };
   response.render("logIn.hbs", model);
 });
-
-app.use(express.static("public"));
 
 app.listen(8080);
